bbac8793bbdb2c3694b75ed866daf7f79c988b2c
Tyrn <alexey1911@gmail.com>
2025-08-24 14:24:24 +0300

    QuestionStore.ts: import QuestionModel from Question

--- src/models/QuestionStore.ts
+++ src/models/QuestionStore.ts
@@ -1,12 +1,15 @@
 import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
 import { withSetPropAction } from "./helpers/withSetPropAction"
+import { QuestionModel } from "./Question"
 
 /**
  * Model description here for TypeScript hints.
  */
 export const QuestionStoreModel = types
   .model("QuestionStore")
-  .props({})
+  .props({
+    questions: types.optional(types.array(QuestionModel), []),
+  })
   .actions(withSetPropAction)
   .views((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
   .actions((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
@@ -15,5 +18,3 @@ export interface QuestionStore extends Instance<typeof QuestionStoreModel> {}
 export interface QuestionStoreSnapshotOut extends SnapshotOut<typeof QuestionStoreModel> {}
 export interface QuestionStoreSnapshotIn extends SnapshotIn<typeof QuestionStoreModel> {}
 export const createQuestionStoreDefaultModel = () => types.optional(QuestionStoreModel, {})
-
-
